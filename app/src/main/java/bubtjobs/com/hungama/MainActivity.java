7375003c74545076365d9f952e30783024863227
jalilcse03 <jalil_cse@yahoo.com>
2016-05-09 11:20:11 +0600

    home page working

---
+++ app/src/main/java/bubtjobs/com/hungama/MainActivity.java
@@ -0,0 +1,21 @@
+package bubtjobs.com.hungama;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button go_bt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void go(View view) {
+        startActivity(new Intent(MainActivity.this,Home.class));
+    }
+}
